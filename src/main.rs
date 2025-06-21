845c1f8d239732d2daef329e51233d64b383063b
hagaitski <i.haga629@gmail.com>
2025-06-22 01:19:36 +0900

    HelloTextコンポーネントを追加し、シフトキー入力に応じてテキストを変更する機能を実装

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,47 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct HelloText;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, handle_shift_key)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    
+    commands.spawn((
+        Text::new("Hello Bevy"),
+        TextFont {
+            font_size: 60.0,
+            ..default()
+        },
+        TextColor(Color::WHITE),
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Percent(45.0),
+            left: Val::Percent(35.0),
+            ..default()
+        },
+        HelloText,
+    ));
+}
+
+fn handle_shift_key(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut text_query: Query<&mut Text, With<HelloText>>,
+) {
+    if keyboard_input.pressed(KeyCode::ShiftLeft) || keyboard_input.pressed(KeyCode::ShiftRight) {
+        for mut text in text_query.iter_mut() {
+            text.0 = "Thank you".to_string();
+        }
+    } else {
+        for mut text in text_query.iter_mut() {
+            text.0 = "Hello Bevy".to_string();
+        }
+    }
 }
